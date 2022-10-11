839488ed36f77358732be1c348e1839254327118
Jyotirmoy <subhodas45036@gmail.com>
2022-10-11 22:06:41 +0530

    Initial Commit - Completed Upto Landing Page

---
+++ src/components/category-item/category-item-component.jsx
@@ -0,0 +1,21 @@
+import './category-item.styles.css'
+
+const CategoryItem = ({category}) => {
+    let {title, image} = category;
+    return (
+        <div className="category-container" style={
+            {
+              backgroundImage: `url(${image})`,
+              backgroundSize: 'cover'
+            }
+          }>
+          {/* <img/> */}
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+    )
+}
+
+export default CategoryItem;
